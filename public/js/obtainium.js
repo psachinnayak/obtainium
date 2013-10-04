c1f6b728c22c890f16526070aa1d7c6c84bd8aa4
Naresh Jain <nashjain@gmail.com>
2013-10-04 20:12:34 +0530

    removed duplicate body selector

--- public/js/obtainium.js
+++ public/js/obtainium.js
@@ -1,10 +1,11 @@
 // Focus the first input field in a modal, if it exists.
-$("body").delegate(".modal", "shown.bs.modal", function(evt) {
+var body = $("body");
+body.delegate(".modal", "shown.bs.modal", function(evt) {
 	$("input[type!='hidden']", evt.currentTarget).get(0).focus();
 });
 
 // Handle form uploads using XHR
-$("body").delegate("form", "submit", function(evt) {
+body.delegate("form", "submit", function(evt) {
 	var form = $(evt.currentTarget);
 
 	$.ajax({
@@ -22,6 +23,6 @@ $("body").delegate("form", "submit", function(evt) {
 });
 
 // Reset the form when the modal is hidden
-$("body").delegate(".modal", "hidden.bs.modal", function(evt) {
+body.delegate(".modal", "hidden.bs.modal", function(evt) {
 	$("form").each(function() { this.reset() });
 });
