2047014fe747e82dce9e3069286cc73f6e29cb2a
Rakesh Pai <rakeshpai@gmail.com>
2013-10-04 20:10:20 +0530

    Forms publish an event on success now

--- public/js/obtainium.js
+++ public/js/obtainium.js
@@ -1,9 +1,9 @@
 // Focus the first input field in a modal, if it exists.
-
 $("body").delegate(".modal", "shown.bs.modal", function(evt) {
 	$("input[type!='hidden']", evt.currentTarget).get(0).focus();
 });
 
+// Handle form uploads using XHR
 $("body").delegate("form", "submit", function(evt) {
 	var form = $(evt.currentTarget);
 
@@ -12,11 +12,16 @@ $("body").delegate("form", "submit", function(evt) {
 		type: form.attr("method"),
 		data: form.serialize(),
 		dataType: "json",
-		success: function(data) {
+		success: function() {
 			form.parents(".modal").modal("hide");
-			console.log("Done", data);
+			$("body").trigger(form.attr("data-success"), arguments)
 		}
 	});
 
 	evt.preventDefault();
+});
+
+// Reset the form when the modal is hidden
+$("body").delegate(".modal", "hidden.bs.modal", function(evt) {
+	$("form").each(function() { this.reset() });
 });
