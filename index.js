2784fe1a33f360684f365230696123378fe4a476
Rakesh Pai <rakeshpai@gmail.com>
2013-10-04 16:10:27 +0530

    Added express boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+app.use(function(err, req, res, next){
+  console.log(err.stack);
+  res.send(500, "Something broke!");
+});
+
 app.get("/", function(req, res) {
 	res.send("Hello world");
 });
